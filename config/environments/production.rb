84194b299e472e244d34406a464d420366f7c0e0
DJ Greenfield <mr.kaitryn.dale@gmail.com>
2020-12-04 11:17:32 -0700

    Add rebrandly as a url shortener library.
    It was the first library I came across with a very small setup. Just
    need to sign up and get an API key.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,5 +102,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.expertise_generator = 'ExpertiseGenerator'
+  config.expertise_generator  = 'ExpertiseGenerator'
+  config.url_shortener        = 'UrlShortener'
+  config.url_shortener_client = 'Rebrandly::Api'
 end
