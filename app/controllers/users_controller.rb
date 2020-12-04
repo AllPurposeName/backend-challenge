08731391b7922410d2321eee755420e616b96bed
DJ Greenfield <mr.kaitryn.dale@gmail.com>
2020-12-04 11:44:51 -0700

    Add show and index endpoints with new serializers
    I'm not adding test to this because at this point I don't think it can
    fail really... yet

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,25 @@
 class UsersController < ApplicationController
+
+  def show
+    user      = User.find_by(id: params[:id])
+    blueprint = UserBlueprint.render(user, view: :normal)
+
+    render json: blueprint, status: 200
+  end
+
+  def index
+    users     = User.all
+    blueprint = UserBlueprint.render(users, view: :compact)
+
+    render json: blueprint, status: 200
+  end
+
   def create
     user = UserService.create!(
       name: strong_params[:name],
       personal_website: strong_params[:personal_website]
     )
-    blueprint = UserBlueprint.render(user)
+    blueprint = UserBlueprint.render(user, view: :normal)
 
     render json: blueprint, status: 201
   rescue ApiError::BasicError => error
