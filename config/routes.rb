067b5f80ccc7bd4c16bee7b2c315bc96301ba29f
DJ Greenfield <mr.kaitryn.dale@gmail.com>
2020-12-04 17:08:10 -0700

    Adding a find experts rpc endpoint.
    I stick to REST when I can and when it makes sense. The more I learn
    about REST, the more I learn not to shoehorn every single thing into it.
    
    We could treat expert like their own full fledged resource, or we could
    nest them under /users/. I choose neither and picked the thing that made
    the most sense to me. Use the user controller, give expert it's own
    namespace in the api, and give it a unique shape (serializer). I think
    treating `find_experts` as a first class citizen is important. It is
    theheart of the app and I wouldn't like to debilitate it by forcing it
    into a rest box that might not be intuitive to integrate with.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:create, :index, :show]
   resources :friendships, only: [:create]
+  get :find_experts, controller: :users
 end
